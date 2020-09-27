3a934ead7dbde944cf00563c26f17650b9783aa1
Khalil Najjar <knajjars@gmail.com>
2020-09-27 09:55:25 +0200

    Updates

--- src/env/env-variables.ts
+++ src/env/env-variables.ts
@@ -1,5 +1,6 @@
 export const JWT_KEY = process.env.JWT_KEY as string
 export const NODE_ENV = process.env.NODE_ENV as string
+export const MONGO_URI = process.env.MONGO_URI as string
 
 if (typeof JWT_KEY === 'undefined') {
   throw new Error("Missing env variable 'JWT_KEY'")
@@ -7,4 +8,8 @@ if (typeof JWT_KEY === 'undefined') {
 
 if (typeof NODE_ENV === 'undefined') {
   throw new Error("Missing env variable 'NODE_ENV'")
+}
+
+if (typeof MONGO_URI === 'undefined') {
+  throw new Error("Missing env variable 'MONGO_URI'")
 }
